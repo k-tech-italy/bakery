cd661a348ba5a9fd3c9445eafa764c9bff4bd092
Giovanni Bronzini <g.bronzini@gmail.com>
2025-02-27 09:23:11 +0000

    Added Django Tailwind4

---
+++ kt-django-tw4/{{cookiecutter.project__slug}}/src/{{cookiecutter.project__module}}/__init__.py
@@ -0,0 +1,5 @@
+__author__ = '{{cookiecutter.project__author}}'
+__version__ = '0.1.0'
+__name__ = '{{cookiecutter.project__slug}}'
+VERSION = __version__
+NAME = __name__
---
+++ kt-django-tw4/{{cookiecutter.project__slug}}/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ['./src/{{cookiecutter.project__module}}/*.html'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
